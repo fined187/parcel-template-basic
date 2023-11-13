9a289c8f713b2ee816d1b69d8fcec9a51bc71f47
fined000@naver.com <fined000@naver.com>
2023-11-13 17:32:05 +0900

    create commit

---
+++ .postcssrc.js
@@ -0,0 +1,11 @@
+//  ESM(EcmaScript Module) 형태로 export (import 사용 가능)
+//  CommonJS 형태로 export (require 사용 가능)
+
+// import => require
+
+// export => module.exports
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
